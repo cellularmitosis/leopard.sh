74c6d58b0eaff0836c0100992127bdf7b0d60429
Jason Pepas <jasonpepas@gmail.com>
2022-02-10 18:03:22 -0600

    use a tempfile when making a binpkg

--- leopardsh/utils/make-leopardsh-binpkg.sh
+++ leopardsh/utils/make-leopardsh-binpkg.sh
@@ -3,9 +3,12 @@
 set -e -x
 
 pkgspec=$1
-cd /opt
 binpkg=$pkgspec.$(leopard.sh --os.cpu).tar.gz
+
 LEOPARDSH_BINPKG_PATH=${LEOPARDSH_BINPKG_PATH:-~/Desktop/leopardsh/binpkgs}
 mkdir -p $LEOPARDSH_BINPKG_PATH
-tar czf /tmp/$binpkg $pkgspec
-mv /tmp/$binpkg $LEOPARDSH_BINPKG_PATH/
+
+cd /opt
+tmpfile=$(mktemp /tmp/binpkg.XXXX)
+tar czf $tmpfile $pkgspec
+mv $tmpfile $LEOPARDSH_BINPKG_PATH/$binpkg
--- tigersh/utils/make-tigersh-binpkg.sh
+++ tigersh/utils/make-tigersh-binpkg.sh
@@ -3,9 +3,12 @@
 set -e -x
 
 pkgspec=$1
-cd /opt
 binpkg=$pkgspec.$(tiger.sh --os.cpu).tar.gz
+
 TIGERSH_BINPKG_PATH=${TIGERSH_BINPKG_PATH:-~/Desktop/tigersh/binpkgs}
 mkdir -p $TIGERSH_BINPKG_PATH
-tar czf /tmp/$binpkg $pkgspec
-mv /tmp/$binpkg $TIGERSH_BINPKG_PATH/
+
+cd /opt
+tmpfile=$(mktemp /tmp/binpkg.XXXX)
+tar czf $tmpfile $pkgspec
+mv $tmpfile $TIGERSH_BINPKG_PATH/$binpkg
