bc5f9978f439f14aaac34813890c8258354dc3e6
Jason Pepas <jasonpepas@gmail.com>
2022-02-15 23:55:35 -0600

    wip

--- leopardsh/scripts/install-git-2.34.1.sh
+++ leopardsh/scripts/install-git-2.34.1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# based on templates/build-from-source.sh v4
+
 
 # Install git on OS X Leopard / PowerPC.
 
@@ -15,21 +17,19 @@ fi
 
 pkgspec=$package-$version$ppc64
 
-if ! which -s xz ; then
-    leopard.sh xz-5.2.5$ppc64
-fi
+for dep in \
+    expat-2.4.3$ppc64 \
+    libiconv-1.16$ppc64 \
+    libressl-3.4.2$ppc64 \
+    xz-5.2.5$ppc64
+do
+    if ! test -e /opt/$dep ; then
+        leopard.sh $dep
+    fi
+    CPPFLAGS="-I/opt/$dep/include $CPPFLAGS"
+    LDFLAGS="-L/opt/$dep/lib $LDFLAGS"
+done
 
-if ! test -e /opt/libressl-3.4.2$ppc64 ; then
-    leopard.sh libressl-3.4.2$ppc64
-fi
-
-if ! test -e /opt/libiconv-1.16$ppc64 ; then
-    leopard.sh libiconv-1.16$ppc64
-fi
-
-if ! test -e /opt/expat-2.4.3$ppc64 ; then
-    leopard.sh expat-2.4.3$ppc64
-fi
 
 echo -n -e "\033]0;leopard.sh $pkgspec ($(leopard.sh --os.cpu))\007"
 
@@ -46,12 +46,20 @@ else
         curl -#fLO $srcmirror/$tarball
     fi
 
+    test "$(md5 ~/Downloads/$tarball | awk '{print $NF}')" = f442dade3c73ea39473f6700b3e04dcd
+
     cd /tmp
     rm -rf $package-$version
     cat ~/Downloads/$tarball | unxz | tar x
     cd $package-$version
 
     cat /opt/leopard.sh/share/leopard.sh/config.cache/leopard.cache > config.cache
+
+    CFLAGS=$(leopard.sh -mcpu -O)
+    if test -n "$ppc64" ; then
+        CFLAGS="-m64 $CFLAGS"
+        LDFLAGS="-m64 $LDFLAGS"
+    fi
 
     # FIXME despite all of these flags, git is still missing a curl symbol:
     #     LINK git-http-fetch
@@ -61,16 +69,21 @@ else
     # ld: symbol(s) not found
     # collect2: ld returned 1 exit status
     # make: *** [git-http-fetch] Error 1
-    CPPFLAGS=-I/opt/portable-curl-7.58.0-1/include \
-    LDFLAGS=-L/opt/portable-curl-7.58.0-1/lib \
-    LIBS=-lcurl \
+    CPPFLAGS="$CPPFLAGS -I/opt/portable-curl-7.58.0-1/include"
+    LDFLAGS="$LDFLAGS -L/opt/portable-curl-7.58.0-1/lib"
+    LIBS="$LIBS -lcurl"
+
         ./configure -C --prefix=/opt/$pkgspec \
             --with-openssl=/opt/libressl-3.4.2$ppc64 \
             --with-curl=/opt/portable-curl-7.58.0-1 \
             --with-iconv=/opt/libiconv-1.16$ppc64 \
-            --with-expat=/opt/expat-2.4.3$ppc64
+            --with-expat=/opt/expat-2.4.3$ppc64 \
+            CFLAGS="$CFLAGS" \
+            CPPFLAGS="$CPPFLAGS" \
+            LDFLAGS="$LDFLAGS" \
+            LIBS="$LIBS"
 
-    make $(leopard.sh -j)
+    make $(leopard.sh -j) V=1
 
     if test -n "$LEOPARDSH_RUN_TESTS" ; then
         # FIXME some tests need a recent python.
