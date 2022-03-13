53ba3afdd9743c46e3529b577ee74b38ff052594
Jason Pepas <jasonpepas@gmail.com>
2022-03-13 10:55:11 -0500

    iceweaselppc.app-fpr13

---
+++ leopardsh/scripts/install-iceweaselppc.app-fpr13.sh
@@ -0,0 +1,56 @@
+#!/opt/tigersh-deps-0.1/bin/bash
+# based on templates/install-app.sh v1
+
+# Install IceWeasel-PPC.app on OS X / PowerPC.
+
+package=iceweaselppc.app
+version=fpr13
+ucversion=FPR13
+appname1=IceWeaselPPC
+upstream_website=https://forums.macrumors.com/threads/so-this-finally-happened-sort-of.2172031/page-3#post-27366588
+
+set -e -o pipefail
+PATH="/opt/tigersh-deps-0.1/bin:$PATH"
+LEOPARDSH_MIRROR=${LEOPARDSH_MIRROR:-https://leopard.sh}
+TIGERSH_MIRROR=${TIGERSH_MIRROR:-https://leopard.sh}
+
+pkgspec=$package-$version
+
+osversion=$(sw_vers -productVersion | awk '{print $NF}')
+if test "${osversion:0:4}" = "10.4" ; then
+    pkgmgr="tiger.sh"
+    mirror=$TIGERSH_MIRROR
+elif test "${osversion:0:4}" = "10.5" ; then
+    pkgmgr="leopard.sh"
+    mirror=$LEOPARDSH_MIRROR
+fi
+test -n "$pkgmgr"
+
+echo -n -e "\033]0;$pkgmgr $pkgspec ($($pkgmgr --cpu))\007"
+
+tarball=$pkgspec.tar.gz
+url=$mirror/dist/$tarball
+echo -e "${COLOR_CYAN}Unpacking${COLOR_NONE} $tarball into /opt." >&2
+$pkgmgr --unpack-tarball-check-md5 $url /opt
+
+echo -e "${COLOR_CYAN}Creating${COLOR_NONE} aliases for $pkgspec." >&2
+for appname in "$appname1" ; do
+    # Note: these must be aliases, symlinks don't work.
+    # Note: if we call this too soon after unpacking, it will fail with:
+    #     29:124: execution error: Finder got an error: The operation could not be completed. (-1407)
+    # So we try it a few times until it succeeds.  So gross!
+    for i in 1 2 3 4 5 ; do
+        aliasname=$(
+            osascript -e "tell application \"Finder\" to make alias file to POSIX file \"/opt/$pkgspec/$appname.app\" at POSIX file \"/opt/$pkgspec\"" 2>/dev/null || true
+        )
+        if test -z "$aliasname" ; then
+            sleep 1
+            continue
+        fi
+        aliasname=$( echo $aliasname | sed 's/^alias file //' )
+        rm -f "/Applications/$appname $ucversion"
+        mv "/opt/$pkgspec/$aliasname" "/Applications/$appname $ucversion"
+        echo "  \"/Applications/$appname $ucversion\" -> /opt/$pkgspec/$appname"
+        break
+    done
+done
---
+++ tigersh/scripts/install-iceweaselppc.app-fpr13.sh
@@ -0,0 +1,1 @@
+../../leopardsh/scripts/install-iceweaselppc.app-fpr13.sh
