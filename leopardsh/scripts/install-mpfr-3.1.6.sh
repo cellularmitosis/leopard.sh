0df9070639040507a8d60cbf9a02418dc0cefe67
Jason Pepas <jasonpepas@gmail.com>
2022-03-12 18:17:09 -0600

    mpfr-3.1.6

--- leopardsh/scripts/install-mpfr-3.1.6.sh
+++ leopardsh/scripts/install-mpfr-3.1.6.sh
@@ -1,7 +1,7 @@
-#!/opt/tigersh-deps-0.1/bin/bash
-# based on templates/install-foo-1.0.sh v4
+#!/bin/bash
+# based on templates/build-from-source.sh v6
 
-# Install mpfr on OS X Tiger / PowerPC.
+# Install mpfr on OS X Leopard / PowerPC.
 
 package=mpfr
 version=3.1.6
@@ -9,7 +9,7 @@ upstream=https://ftp.gnu.org/gnu/$package/$package-$version.tar.gz
 
 set -e -o pipefail
 PATH="/opt/tigersh-deps-0.1/bin:$PATH"
-TIGERSH_MIRROR=${TIGERSH_MIRROR:-https://leopard.sh}
+LEOPARDSH_MIRROR=${LEOPARDSH_MIRROR:-https://leopard.sh}
 
 if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
     ppc64=".ppc64"
@@ -21,15 +21,15 @@ for dep in \
     gmp-4.3.2$ppc64
 do
     if ! test -e /opt/$dep ; then
-        tiger.sh $dep
+        leopard.sh $dep
     fi
     CPPFLAGS="-I/opt/$dep/include $CPPFLAGS"
     LDFLAGS="-L/opt/$dep/lib $LDFLAGS"
 done
 
-echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --os.cpu))\007"
+echo -n -e "\033]0;leopard.sh $pkgspec ($(leopard.sh --os.cpu))\007"
 
-if tiger.sh --install-binpkg $pkgspec ; then
+if leopard.sh --install-binpkg $pkgspec ; then
     exit 0
 fi
 
@@ -37,14 +37,14 @@ echo -e "${COLOR_CYAN}Building${COLOR_NONE} $pkgspec from source." >&2
 set -x
 
 if ! test -e /usr/bin/gcc ; then
-    tiger.sh xcode-2.5
+    leopard.sh xcode-3.1.4
 fi
 
-tiger.sh --unpack-dist $pkgspec
+leopard.sh --unpack-dist $pkgspec
 cd /tmp/$package-$version
 
 
-CFLAGS=" $(tiger.sh -mcpu -O) -Wall -Wmissing-prototypes -Wpointer-arith"
+CFLAGS=" $(leopard.sh -mcpu -O) -Wall -Wmissing-prototypes -Wpointer-arith"
 if test -n "$ppc64" ; then
     CFLAGS="-m64 $CFLAGS"
 fi
@@ -55,9 +55,9 @@ fi
     --with-gmp=/opt/gmp-4.3.2$ppc64 \
     CFLAGS="$CFLAGS"
 
-/usr/bin/time make $(tiger.sh -j)
+/usr/bin/time make $(leopard.sh -j)
 
-if test -n "$TIGERSH_RUN_BROKEN_TESTS" ; then
+if test -n "$LEOPARDSH_RUN_BROKEN_TESTS" ; then
     # Note: 1 failing test:
     #   PASS: toutimpl
     #   ../test-driver: line 107: 60863 Segmentation fault      "$@" > $log_file 2>&1
@@ -66,11 +66,11 @@ fi
 
 make install
 
-tiger.sh --linker-check $pkgspec
-tiger.sh --arch-check $pkgspec $ppc64
+leopard.sh --linker-check $pkgspec
+leopard.sh --arch-check $pkgspec $ppc64
 
 if test -e config.cache ; then
-    mkdir -p /opt/$pkgspec/share/tiger.sh/$pkgspec
+    mkdir -p /opt/$pkgspec/share/leopard.sh/$pkgspec
     gzip -9 config.cache
-    mv config.cache.gz /opt/$pkgspec/share/tiger.sh/$pkgspec/
+    mv config.cache.gz /opt/$pkgspec/share/leopard.sh/$pkgspec/
 fi
