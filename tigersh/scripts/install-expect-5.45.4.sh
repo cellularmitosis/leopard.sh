21443382db0cdd820ecb2d7cb2da77521b74cc77
Jason Pepas <jasonpepas@gmail.com>
2022-03-19 16:44:25 -0500

    fixing expect-5.45.4

---
+++ tigersh/scripts/install-expect-5.45.4.sh
@@ -0,0 +1,66 @@
+#!/opt/tigersh-deps-0.1/bin/bash
+# based on templates/build-from-source.sh v6
+
+# Install expect on OS X Tiger / PowerPC.
+
+package=expect
+version=5.45.4
+upstream=http://prdownloads.sourceforge.net/expect/expect$version.tar.gz
+
+set -e -o pipefail
+PATH="/opt/tigersh-deps-0.1/bin:$PATH"
+TIGERSH_MIRROR=${TIGERSH_MIRROR:-https://leopard.sh}
+
+if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
+    ppc64=".ppc64"
+fi
+
+pkgspec=$package-$version$ppc64
+
+echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --cpu))\007"
+
+if tiger.sh --install-binpkg $pkgspec ; then
+    exit 0
+fi
+
+echo -e "${COLOR_CYAN}Building${COLOR_NONE} $pkgspec from source." >&2
+set -x
+
+if ! test -e /usr/bin/gcc ; then
+    tiger.sh xcode-2.5
+fi
+
+if ! test -e /opt/tcl-8.6.12 ; then
+    tiger.sh tcl-8.6.12
+fi
+
+echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --cpu))\007"
+
+tiger.sh --unpack-dist $pkgspec
+cd /tmp/$package-$version
+
+CFLAGS=$(tiger.sh -mcpu -O)
+if test -n "$ppc64" ; then
+    CFLAGS="-m64 $CFLAGS"
+fi
+
+/usr/bin/time ./configure -C --prefix=/opt/$pkgspec \
+    --with-tcl=/opt/tcl-8.6.12/lib \
+    CFLAGS="$CFLAGS"
+
+/usr/bin/time make $(tiger.sh -j) V=1
+
+if test -n "$TIGERSH_RUN_TESTS" ; then
+    make test
+fi
+
+make install
+
+tiger.sh --linker-check $pkgspec
+tiger.sh --arch-check $pkgspec $ppc64
+
+if test -e config.cache ; then
+    mkdir -p /opt/$pkgspec/share/tiger.sh/$pkgspec
+    gzip -9 config.cache
+    mv config.cache.gz /opt/$pkgspec/share/tiger.sh/$pkgspec/
+fi
