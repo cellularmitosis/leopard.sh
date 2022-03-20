53918b1f3b44e81229240c735333c9002d7055a1
Jason Pepas <jasonpepas@gmail.com>
2022-03-20 00:22:37 -0500

    coreutils-9.0

---
+++ leopardsh/scripts/install-coreutils-9.0.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# based on templates/build-from-source.sh v5
+
+# Install coreutils on OS X Leopard / PowerPC.
+
+package=coreutils
+version=9.0
+upstream=https://ftp.gnu.org/gnu/$package/$package-$version.tar.gz
+
+set -e -o pipefail
+PATH="/opt/tigersh-deps-0.1/bin:$PATH"
+LEOPARDSH_MIRROR=${LEOPARDSH_MIRROR:-https://leopard.sh}
+
+if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
+    ppc64=".ppc64"
+fi
+
+pkgspec=$package-$version$ppc64
+
+echo -n -e "\033]0;leopard.sh $pkgspec ($(leopard.sh --cpu))\007"
+
+if leopard.sh --install-binpkg $pkgspec ; then
+    exit 0
+fi
+
+echo -e "${COLOR_CYAN}Building${COLOR_NONE} $pkgspec from source." >&2
+set -x
+
+if ! test -e /usr/bin/gcc ; then
+    leopard.sh xcode-3.1.4
+fi
+
+leopard.sh --unpack-dist $pkgspec
+cd /tmp/$package-$version
+
+CFLAGS=$(leopard.sh -mcpu -O)
+if test -n "$ppc64" ; then
+    CFLAGS="-m64 $CFLAGS"
+fi
+
+/usr/bin/time ./configure -C --prefix=/opt/$pkgspec \
+    CFLAGS="$CFLAGS"
+
+# On Leopard, coreutils attempts to build libstdbuf, but it fails due to bad
+# flags (i.e. -shared instead of -dynamiclib, etc).  Disable it:
+perl -pi -e "s|pkglibexec_PROGRAMS = src/libstdbuf.so|pkglibexec_PROGRAMS = |" Makefile
+
+/usr/bin/time make $(leopard.sh -j) V=1
+
+if test -n "$LEOPARDSH_RUN_TESTS" ; then
+    make check
+fi
+
+make install
+
+leopard.sh --linker-check $pkgspec
+leopard.sh --arch-check $pkgspec $ppc64
+
+if test -e config.cache ; then
+    mkdir -p /opt/$pkgspec/share/leopard.sh/$pkgspec
+    gzip -9 config.cache
+    mv config.cache.gz /opt/$pkgspec/share/leopard.sh/$pkgspec/
+fi
