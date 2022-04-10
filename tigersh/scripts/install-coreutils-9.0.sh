477e2f4b8d54a88a2f3ac2ded2bf942db19a97fb
Jason Pepas <jasonpepas@gmail.com>
2022-04-09 20:33:20 -0500

    coreutils-9.0

--- tigersh/scripts/install-coreutils-9.0.sh
+++ tigersh/scripts/install-coreutils-9.0.sh
@@ -11,11 +11,26 @@ set -e -o pipefail
 PATH="/opt/tigersh-deps-0.1/bin:$PATH"
 TIGERSH_MIRROR=${TIGERSH_MIRROR:-https://leopard.sh}
 
-if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
-    ppc64=".ppc64"
-fi
+# note: fails to build on ppc64.
+#if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
+#    ppc64=".ppc64"
+#fi
 
 pkgspec=$package-$version$ppc64
+
+for dep in \
+    gettext-0.20$ppc64 \
+    gmp-4.3.2$ppc64 \
+    libiconv-1.16$ppc64 \
+    libressl-3.4.2$ppc64
+do
+    if ! test -e /opt/$dep ; then
+        tiger.sh $dep
+    fi
+    CPPFLAGS="-I/opt/$dep/include $CPPFLAGS"
+    LDFLAGS="-L/opt/$dep/lib $LDFLAGS"
+    PATH="/opt/$dep/bin:$PATH"
+done
 
 echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --cpu))\007"
 
@@ -38,8 +53,31 @@ if test -n "$ppc64" ; then
     CFLAGS="-m64 $CFLAGS"
 fi
 
+# Note: coreutils ends up linking against /usr/lib/libiconv, despite having
+# used --with-libiconv-prefix=/opt/libiconv...:
+#   $ otool -L /opt/coreutils-9.0/bin/* | grep /usr/lib/libiconv
+#   /usr/lib/libiconv.2.dylib (compatibility version 7.0.0, current version 7.0.0)
+# My guess is that this is some bad interaction between --with-libiconv-prefix and
+# --with-libintl-prefix, based on this configure output:
+#   checking how to link with libiconv... -L/opt/libiconv-1.16/lib -liconv
+#   checking how to link with libintl... -L/opt/gettext-0.21/lib -lintl -liconv -Wl,-framework -Wl,CoreFoundation
+# You end up with some correct lines:
+#   gcc-4.2 -std=gnu99   -mcpu=7450 -O2   -o src/factor src/factor.o src/libver.a lib/libcoreutils.a -L/opt/gettext-0.21/lib -lintl -liconv -Wl,-framework -Wl,CoreFoundation lib/libcoreutils.a  -L/opt/libiconv-1.16/lib -liconv 
+# and some which are incorrect:
+#   gcc-4.2 -std=gnu99   -mcpu=7450 -O2   -o src/chroot src/chroot.o src/libver.a lib/libcoreutils.a -L/opt/gettext-0.21/lib -lintl -liconv -Wl,-framework -Wl,CoreFoundation lib/libcoreutils.a 
+# Coreutils alse ends up linking against /usr/lib/libcrypto:
+#   $ otool -L /opt/coreutils-9.0/bin/* | grep /usr/lib/libcrypto
+#   /usr/lib/libcrypto.0.9.7.dylib (compatibility version 0.9.7, current version 0.9.7)
+# So we need to use CPPFLAGS and LDFLAGS to overcome this.
+
 /usr/bin/time ./configure -C --prefix=/opt/$pkgspec \
-    CFLAGS="$CFLAGS"
+    --with-openssl=yes \
+    --with-libiconv-prefix=/opt/libiconv-1.16$ppc64 \
+    --with-libgmp-prefix=/opt/gmp-4.3.2$ppc64 \
+    --with-libintl-prefix=/opt/gettext-0.20$ppc64 \
+    CFLAGS="$CFLAGS" \
+    CPPFLAGS="$CPPFLAGS" \
+    LDFLAGS="$LDFLAGS"
 
 /usr/bin/time make $(tiger.sh -j) V=1
 
