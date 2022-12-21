fbd0118cda6dacaee6df5e84e7a00c64266da443
Jason Pepas <jasonpepas@gmail.com>
2022-12-21 03:03:45 -0600

    libtool-2.4.6

---
+++ leopardsh/scripts/install-libtool-2.4.6.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# based on templates/build-from-source.sh v6
+
+# Install libtool on OS X Leopard / PowerPC.
+
+package=libtool
+version=2.4.6
+upstream=https://ftp.gnu.org/gnu/$package/$package-$version.tar.gz
+
+set -e -o pipefail
+PATH="/opt/tigersh-deps-0.1/bin:$PATH"
+LEOPARDSH_MIRROR=${LEOPARDSH_MIRROR:-https://leopard.sh}
+
+if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
+    ppc64=".ppc64"
+fi
+
+pkgspec=$package-$version$ppc64
+
+if ! test -e /opt/m4-1.4.19$ppc64 ; then
+    leopard.sh m4-1.4.19$ppc64
+fi
+
+echo -n -e "\033]0;leopard.sh $pkgspec ($(leopard.sh --cpu))\007"
+
+if leopard.sh --install-binpkg $pkgspec ; then
+    exit 0
+fi
+
+echo -e "${COLOR_CYAN}Building${COLOR_NONE} $pkgspec from source." >&2
+set -x
+
+if ! test -e /usr/bin/gcc ; then
+    leopard.sh xcode-3.1.4
+fi
+
+echo -n -e "\033]0;leopard.sh $pkgspec ($(leopard.sh --cpu))\007"
+
+leopard.sh --unpack-dist $pkgspec
+cd /tmp/$package-$version
+
+CFLAGS=$(leopard.sh -mcpu -O)
+if test -n "$ppc64" ; then
+    CFLAGS="-m64 $CFLAGS"
+    LDFLAGS="-m64 $LDFLAGS"
+fi
+
+/usr/bin/time ./configure -C --prefix=/opt/$pkgspec \
+    LDFLAGS="$LDFLAGS" \
+    CFLAGS="$CFLAGS"
+
+/usr/bin/time make $(leopard.sh -j) V=1
+
+if test -n "$LEOPARDSH_RUN_TESTS" ; then
+    make check
+fi
+
+make install
+
+leopard.sh --linker-check $pkgspec
+leopard.sh --arch-check $pkgspec $ppc64
+
+if test -e config.cache ; then
+    mkdir -p /opt/$pkgspec/share/leopard.sh/$pkgspec
+    gzip -9 config.cache
+    mv config.cache.gz /opt/$pkgspec/share/leopard.sh/$pkgspec/
+fi
---
+++ tigersh/scripts/install-libtool-2.4.6.sh
@@ -0,0 +1,67 @@
+#!/opt/tigersh-deps-0.1/bin/bash
+# based on templates/build-from-source.sh v6
+
+# Install libtool on OS X Tiger / PowerPC.
+
+package=libtool
+version=2.4.6
+upstream=https://ftp.gnu.org/gnu/$package/$package-$version.tar.gz
+
+set -e -o pipefail
+PATH="/opt/tigersh-deps-0.1/bin:$PATH"
+TIGERSH_MIRROR=${TIGERSH_MIRROR:-https://leopard.sh}
+
+if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
+    ppc64=".ppc64"
+fi
+
+pkgspec=$package-$version$ppc64
+
+if ! test -e /opt/m4-1.4.19$ppc64 ; then
+    tiger.sh m4-1.4.19$ppc64
+fi
+
+echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --cpu))\007"
+
+if tiger.sh --install-binpkg $pkgspec ; then
+    exit 0
+fi
+
+echo -e "${COLOR_CYAN}Building${COLOR_NONE} $pkgspec from source." >&2
+set -x
+
+if ! test -e /usr/bin/gcc ; then
+    tiger.sh xcode-2.5
+fi
+
+echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --cpu))\007"
+
+tiger.sh --unpack-dist $pkgspec
+cd /tmp/$package-$version
+
+CFLAGS=$(tiger.sh -mcpu -O)
+if test -n "$ppc64" ; then
+    CFLAGS="-m64 $CFLAGS"
+    LDFLAGS="-m64 $LDFLAGS"
+fi
+
+/usr/bin/time ./configure -C --prefix=/opt/$pkgspec \
+    LDFLAGS="$LDFLAGS" \
+    CFLAGS="$CFLAGS"
+
+/usr/bin/time make $(tiger.sh -j) V=1
+
+if test -n "$TIGERSH_RUN_TESTS" ; then
+    make check
+fi
+
+make install
+
+tiger.sh --linker-check $pkgspec
+tiger.sh --arch-check $pkgspec $ppc64
+
+if test -e config.cache ; then
+    mkdir -p /opt/$pkgspec/share/tiger.sh/$pkgspec
+    gzip -9 config.cache
+    mv config.cache.gz /opt/$pkgspec/share/tiger.sh/$pkgspec/
+fi
