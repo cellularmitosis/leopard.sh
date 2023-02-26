508fc9a6cfed779c35172d25105655095e6047ce
Jason Pepas <jasonpepas@gmail.com>
2023-02-25 20:37:39 -0600

    speex-1.2.1

---
+++ tigersh/scripts/install-speex-1.2.1.ppc64.sh
@@ -0,0 +1,1 @@
+install-speex-1.2.1.sh
---
+++ tigersh/scripts/install-speex-1.2.1.sh
@@ -0,0 +1,82 @@
+#!/opt/tigersh-deps-0.1/bin/bash
+# based on templates/build-from-source.sh v6
+
+# Install speex on OS X Tiger / PowerPC.
+
+package=speex
+version=1.2.1
+upstream=https://downloads.xiph.org/releases/speex/$package-$version.tar.gz
+description="Audio compression format designed for speech"
+
+set -e -o pipefail
+PATH="/opt/tigersh-deps-0.1/bin:$PATH"
+TIGERSH_MIRROR=${TIGERSH_MIRROR:-https://leopard.sh}
+
+if test -n "$(echo -n $0 | grep '\.ppc64\.sh$')" ; then
+    ppc64=".ppc64"
+fi
+
+pkgspec=$package-$version$ppc64
+
+for dep in \
+    libogg-1.3.5$ppc64
+do
+    if ! test -e /opt/$dep ; then
+        tiger.sh $dep
+    fi
+    CPPFLAGS="-I/opt/$dep/include $CPPFLAGS"
+    LDFLAGS="-L/opt/$dep/lib $LDFLAGS"
+    PATH="/opt/$dep/bin:$PATH"
+done
+LIBS="-logg"
+
+echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --cpu))\007"
+
+if tiger.sh --install-binpkg $pkgspec ; then
+    exit 0
+fi
+
+echo -e "${COLOR_CYAN}Building${COLOR_NONE} $pkgspec from source." >&2
+set -x
+
+if ! test -e /usr/bin/gcc ; then
+    tiger.sh xcode-2.5
+fi
+
+echo -n -e "\033]0;tiger.sh $pkgspec ($(tiger.sh --cpu))\007"
+
+tiger.sh --unpack-dist $pkgspec
+cd /tmp/$package-$version
+
+CFLAGS="$(tiger.sh -mcpu -O) $CFLAGS"
+CXXFLAGS="$(tiger.sh -mcpu -O) $CFLAGS"
+if test -n "$ppc64" ; then
+    CFLAGS="-m64 $CFLAGS"
+    CXXFLAGS="-m64 $CXXFLAGS"
+    LDFLAGS="-m64 $LDFLAGS"
+fi
+
+/usr/bin/time ./configure -C --prefix=/opt/$pkgspec \
+    --disable-dependency-tracking \
+    --disable-maintainer-mode \
+    OGG_CFLAGS="-I/opt/libogg-1.3.5$ppc64/include -L/opt/libogg-1.3.5$ppc64/lib" \
+    OGG_LIBS=-logg \
+    CFLAGS="$CFLAGS" \
+    CXXFLAGS="$CXXFLAGS"
+
+/usr/bin/time make $(tiger.sh -j) V=1
+
+if test -n "$TIGERSH_RUN_TESTS" ; then
+    make check
+fi
+
+make install
+
+tiger.sh --linker-check $pkgspec
+tiger.sh --arch-check $pkgspec $ppc64
+
+if test -e config.cache ; then
+    mkdir -p /opt/$pkgspec/share/tiger.sh/$pkgspec
+    gzip -9 config.cache
+    mv config.cache.gz /opt/$pkgspec/share/tiger.sh/$pkgspec/
+fi
